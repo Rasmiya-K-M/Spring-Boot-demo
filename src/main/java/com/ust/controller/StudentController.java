270d43f86f3f7af946b16c150074e47477c39648
Rasmiya.Moheyeddinkutty <rasmiya.moheyeddinkutty@ust.com>
2021-04-22 23:17:30 +0530

    second commit

--- src/main/java/com/ust/controller/StudentController.java
+++ src/main/java/com/ust/controller/StudentController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/jenkindemo")
 public class StudentController {
 	@GetMapping("/home")
 	public String getHomePage()
